4658e7374c68ce6c105497bc076fcbb838ba2bf0
Yogesh Anup Shirke <yshirke005@gmail.com>
2025-12-30 19:25:39 +0530

    h

---
+++ First-project/First-project/src/main/java/com/example/First_project/FirstProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.First_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
+
+
+
