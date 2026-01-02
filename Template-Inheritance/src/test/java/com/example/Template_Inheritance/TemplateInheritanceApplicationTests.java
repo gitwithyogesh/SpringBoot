b9266d51383e3c5c79eedfc9b717afef5042b12c
Yogesh Anup Shirke <yshirke005@gmail.com>
2026-01-02 20:10:39 +0530

    okk

---
+++ Template-Inheritance/src/test/java/com/example/Template_Inheritance/TemplateInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Template_Inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
